497e6995e81b7c562561506041e6995c2bd25d64
HONG SE HYUN <sese0733@naver.com>
2024-03-27 21:53:46 +0900

    feat: product card 내용 추가

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -5,9 +5,9 @@ const ProductCard = () => {
     <div>
         <img src='https://lp2.hm.com/hmgoepprod?set=source[/45/d7/45d76135b8260f22b9d11263d80adf1938c6d811.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]' alt='' />
         <div>Conscious choice</div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>벨티드 트윌 코트</div>
+        <div>₩99000</div>
+        <div>신제품</div>
     </div>
   )
 }

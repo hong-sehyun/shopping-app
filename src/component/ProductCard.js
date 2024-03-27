1921d21779b9e7bd2c158c2ebeefebf1347dc2a2
HONG SE HYUN <sese0733@naver.com>
2024-03-27 21:36:06 +0900

    feat: navbar 및 product card  컴포넌트 추가

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+        <img src='https://lp2.hm.com/hmgoepprod?set=source[/45/d7/45d76135b8260f22b9d11263d80adf1938c6d811.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]' alt='' />
+        <div>Conscious choice</div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default ProductCard

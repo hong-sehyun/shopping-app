24ea57516f09ed911403836e3bbe147ff80274e3
HONG SE HYUN <sese0733@naver.com>
2024-03-29 21:53:53 +0900

    feat: 상품 상세 페이지 상세 정보 추가

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Col, Row, Button, Form } from 'react-bootstrap';
 
 const ProductDetail = () => {
   let {id} = useParams();
 
   const [product, setProduct] = useState(null);
+//  const [size, setSize] = useState(null);
 
   const getProductDetail = async () => {
     let url = `https://my-json-server.typicode.com/hong-sehyun/shopping-app/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
+    setProduct(data);
   }
 
   useEffect(() => {
@@ -19,13 +21,25 @@ const ProductDetail = () => {
   },[])
   return (
     <Container>
-      <Col>
-        <img src={product?.img}/>
-      </Col>
-      <Col>
-        <div>{product?.title}</div>
-        <div>{product?.price}</div>
-      </Col>
+      <Row>
+        <Col className='product-img'>
+          <img src={product?.img}/>
+        </Col>
+        <Col>
+          <div>{product?.title}</div>
+          <div>₩{product?.price}</div>
+          <div>{product?.choice == true ? "Conscious Choice" : ""}</div>
+          {product?.size && (
+            <Form.Select aria-label="Default select example">
+              <option>사이즈 선택</option>
+              {product.size.map((size) => (
+                <option value={size}>{size}</option>
+              ))}
+            </Form.Select>
+          )}
+          <div><Button variant='danger' type="submit">추가</Button></div>
+        </Col>
+      </Row>
     </Container>
   )
 }

b3891d2edf287e8ecf344019fd71c6cbf0b61b0c
HONG SE HYUN <sese0733@naver.com>
2024-03-29 20:47:46 +0900

    feat: 상품 상세페이지 생성

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { Container, Col, Row } from 'react-bootstrap';
 
 const ProductDetail = () => {
+  let {id} = useParams();
+
+  const [product, setProduct] = useState(null);
+
+  const getProductDetail = async () => {
+    let url = `https://my-json-server.typicode.com/hong-sehyun/shopping-app/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  }
+
+  useEffect(() => {
+    getProductDetail();
+  },[])
   return (
-    <div>ProductDetail</div>
+    <Container>
+      <Col>
+        <img src={product?.img}/>
+      </Col>
+      <Col>
+        <div>{product?.title}</div>
+        <div>{product?.price}</div>
+      </Col>
+    </Container>
   )
 }
 

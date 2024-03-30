4814df17cb263a12758b5ae7343b100cf8c0e3f3
HONG SE HYUN <sese0733@naver.com>
2024-03-30 22:06:56 +0900

    feat: 모바일 버전 navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons'
-import { faSearch, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faSignOutAlt, faBars } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
+
 
 const Navbar = ({ authenticate, setAuthenticate }) => {
     const menuList = ['여성', 'Divided', '남성', '신생아/유아', '아동', 'H&M Home', 'Sale', '지속가능성']
+    const [mobileMenu, setMobileMenu] = useState(false);
 
     const navigate = useNavigate();
 
@@ -18,9 +21,10 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
     };
 
     const search = (event) => {
-        if(event.key === "Enter") {
+        if (event.key === "Enter") {
             let keyword = event.target.value;
             navigate(`/?q=${keyword}`);
+            setMobileMenu(false);
         }
     }
 
@@ -28,10 +32,35 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
         navigate('/');
     }
 
+    const handleClose = () => setMobileMenu(false);
+    const handleShow = () => setMobileMenu(true);
+
+    const menuContent = (
+        <div className='menu-area'>
+            <ul className='menu-list'>
+                {menuList.map((menu, index) => (
+                    <li key={index}>{menu}</li>
+                ))}
+            </ul>
+            <div className='input-area'>
+                <FontAwesomeIcon icon={faSearch} />
+                <input type="text" onKeyPress={(event) => search(event)} />
+            </div>
+        </div>
+    );
+
     return (
         <div>
+            <div className='mobile-menu-icon' onClick={handleShow}>
+                <FontAwesomeIcon icon={faBars} />
+            </div>
+            <Modal show={mobileMenu} onHide={handleClose} animation={true} dialogClassName="modal-90w" aria-labelledby="contained-modal-title-vcenter" centered>
+                <Modal.Body>
+                    {menuContent}
+                </Modal.Body>
+            </Modal>
             <div>
-            {authenticate ? (
+                {authenticate ? (
                     <div className='login-button' onClick={logout}>
                         <FontAwesomeIcon icon={faSignOutAlt} />
                         <div>로그아웃</div>
@@ -45,24 +74,12 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
             </div>
             <div className='nav-section'>
                 <img
-                    width={100} 
+                    width={100}
                     src='https://blog.kakaocdn.net/dn/Yt80C/btqDeJAYUBo/JQbTuukRladq2AUOeqgiEK/img.jpg' alt=''
                     onClick={goToMain}
                 />
             </div>
-            <div className='menu-area'>
-                <ul className='menu-list'>
-                    {menuList.map((menu) => (
-                        <li>{menu}</li>
-                    ))}
-                </ul>
-
-                <div className='input-area'>
-                    <FontAwesomeIcon icon={ faSearch } />
-                    <input type="text" onKeyPress={(event)=>search(event)}/>
-                </div>
-
-            </div>
+            {!mobileMenu && menuContent}
         </div>
     )
 }
